528e5875b9ad1bfa4b297200d1e122f4ea0f4d90
Zane Rankin <zwrankin@gmail.com>
2019-05-15 21:16:01 -0500

    add initial app with search capability

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Form from "./components/Form";
+
+const API_KEY = "b2b519fa5cf544c8da39416925534c84";
+
+class App extends Component {
+  state = {
+    recipes: []
+  }
+
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    const query = `https://www.food2fork.com/api/search?key=${API_KEY}&q=chicken%20breast&count=5`
+    const api_call = await fetch(query);
+
+    const data = await api_call.json();
+    this.setState({ recipes: data.recipes});
+    console.log(this.state.recipes)
+
+  }
+  render() {
+    return(
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Recipe Search</h1>
+        </header>
+        <Form getRecipe={this.getRecipe}/>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
