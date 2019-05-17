1c853c790f958229f78e1f6de611b31b4e3c95c1
Zane Rankin <zwrankin@gmail.com>
2019-05-16 20:25:01 -0500

    save progress that broke the app

--- src/App.js
+++ src/App.js
@@ -1,36 +1,44 @@
-import React, {Component} from 'react';
-import './App.css'
+import React, { Component } from 'react';
+import './App.css';
 
 import Form from "./components/Form";
+import Recipes from "./components/Recipes";
 
 const API_KEY = "b2b519fa5cf544c8da39416925534c84";
 
 class App extends Component {
-  state = {
-    recipes: []
-  }
+    state = {
+        recipes: []
+    }
+    getRecipe = async (e) => {
+        const recipeName = e.target.elements.recipeName.value;
+        e.preventDefault();
+        const api_call = await fetch(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`);
 
-  getRecipe = async (e) => {
-    const recipeName = e.target.elements.recipeName.value;
-    e.preventDefault();
-    const query = `https://www.food2fork.com/api/search?key=${API_KEY}&q=chicken%20breast&count=5`
-    const api_call = await fetch(query);
-
-    const data = await api_call.json();
-    this.setState({ recipes: data.recipes});
-    console.log(this.state.recipes)
-
-  }
-  render() {
-    return(
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Recipe Search</h1>
-        </header>
-        <Form getRecipe={this.getRecipe}/>
-      </div>
-    )
-  }
+        const data = await api_call.json();
+        this.setState({ recipes: data.recipes });
+        console.log(this.state.recipes);
+    }
+    componentDidMount = () => {
+        const json = localStorage.getItem("recipes");
+        const recipes = JSON.parse(json);
+        this.setState({ recipes });
+    }
+    componentDidUpdate = () => {
+        const recipes = JSON.stringify(this.state.recipes);
+        localStorage.setItem("recipes", recipes);
+    }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <h1 className="App-title">Recipe Search</h1>
+                </header>
+                <Form getRecipe={this.getRecipe} />
+                <Recipes recipes={this.state.recipes} />
+            </div>
+        );
+    }
 }
 
-export default App
+export default App;
