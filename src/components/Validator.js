14709583c730d4cf364e4807c646f679edb7f2a7
Zane Rankin <zwrankin@gmail.com>
2019-05-17 09:05:32 -0500

    conditional text coloring

--- src/components/Validator.js
+++ src/components/Validator.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Validator = props => (
-    <div className="frankie" >
+    <div className="frankie" style={{color: props.query.toLowerCase() === "fish" ? "green" : "red"}}>
         { props.query.toLowerCase() === "fish" ? "Yummmm" : "I WANT FISH"}
     </div>
 );
